03468772f67678e62fe2ac58265b6002f29e713e
srividya-T <vidyachowdary7@gmail.com>
2022-11-15 16:38:23 +0530

    e-commerce

---
+++ React-js/e-commerce/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import { Switch, Route } from "react-router-dom";
+import Products from "./component/Products";
+import Product from "./component/Product";
+
+function App() {
+  return (
+    <>
+    <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/products" component={Products}/>    
+        <Route exact path="/products/:id" component={Product}/>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
